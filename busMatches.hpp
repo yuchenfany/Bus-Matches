e86f655cc56c6e1d86abe831121e4027bcae8b5b
yuchenfany <yuchenfany@gmail.com>
2019-12-16 16:26:38 -0800

    Create busMatches.hpp

---
+++ busMatches.hpp
@@ -0,0 +1,14 @@
+// busMatches.hpp
+
+#ifndef __BUS_MATCHES_HPP
+#define __BUS_MATCHES_HPP
+
+#include <map>
+#include <iostream>
+
+#include "LLQueue.hpp"
+
+
+std::map<int, int> assignBusSeats(std::istream & in);
+
+#endif 
